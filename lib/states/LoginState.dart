989e8bc443f7fd257d6ba8cd3f612f5a724e9ccb
Luis Gonzalez <lgonzalez@ioet.com>
2020-01-21 17:29:04 -0500

    Login with google

--- lib/states/LoginState.dart
+++ lib/states/LoginState.dart
@@ -1,18 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class LoginState with ChangeNotifier {
-  bool _loggedIn = false;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  bool _loggedIn = false;
+  bool _loading = false;
+  FirebaseUser _user;
 
   bool isLoggedIn() => _loggedIn;
+  bool isLoading() => _loading;
+  FirebaseUser getCurrentUser() => _user;
 
-  void login() {
-    _loggedIn = true;
+  void login() async {
+    _loading = true;
+    notifyListeners();
+
+    _user = await _handleSignIn();
+    _loading = false;
+    _loggedIn = _user != null;
     notifyListeners();
   }
 
   void logout() {
+    _googleSignIn.signOut();
     _loggedIn = false;
     notifyListeners();
+  }
+
+  Future<FirebaseUser> _handleSignIn() async {
+    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+    print("signed in " + googleAuth.accessToken);
+
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    final FirebaseUser user =
+        (await _auth.signInWithCredential(credential)).user;
+    print("signed in " + user.displayName);
+    return user;
   }
 }
